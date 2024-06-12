6c1d088a1be1bfca94bcf07d7859152b33467faa
Cristian G <crttngr@gmail.com>
2024-06-12 16:00:41 +0100

    Generar comentarios para JavaDoc de la clase Salmon

--- src/Zoo/Salmon.java
+++ src/Zoo/Salmon.java
@@ -1,33 +1,62 @@
-// Apartado desarrollado por Cristian M Gonzáelz Ramos
-
 package Zoo;
 
 import java.util.Random;
 
+/**
+ * Clase que representa a un salmón en un zoológico.
+ * Extiende la clase Pez.
+ * @author Cristian M González Ramos
+ */
 public class Salmon extends Pez {
+    /** Etapa de vida actual del salmón. */
     String etapaDeVida;
+    /** Indica si el salmón ha migrado o no. */
     boolean haMigrado;
 
+    /** Arreglo que contiene las distintas etapas de vida de un salmón. */
     String[] estapasDeVida = {"Alevín", "Juvenil", "Adulto"};
 
+    /** Generador de números aleatorios. */
     Random rd = new Random();
 
+    /**
+     * Constructor por defecto de la clase Salmon.
+     * Inicializa la etapa de vida y si el salmón ha migrado aleatoriamente.
+     */
     public Salmon() {
         super();
         this.etapaDeVida = estapasDeVida[rd.nextInt(estapasDeVida.length)];
         this.haMigrado = rd.nextBoolean();
     }
 
+    /**
+     * Constructor de la clase Salmon.
+     * @param nombre Nombre del salmón.
+     * @param edad Edad del salmón.
+     * @param peso Peso del salmón.
+     * @param tamaño Tamaño del salmón.
+     * @param colorescamas Colores de las escamas del salmón.
+     * @param tipoAgua Tipo de agua en el que vive el salmón.
+     * @param etapaDeVida Etapa de vida del salmón.
+     * @param haMigrado Indica si el salmón ha migrado.
+     */
     public Salmon(String nombre, int edad, float peso, String tamaño, String colorescamas, String tipoAgua, String etapaDeVida, boolean haMigrado) {
         super(nombre, edad, peso, tamaño, colorescamas, tipoAgua);
         this.etapaDeVida = etapaDeVida;
         this.haMigrado = haMigrado;
     }
 
+    /**
+     * Método que simula la migración del salmón.
+     */
     public void migrar() {
         System.out.println(nombre + " abandona el mar y comienza su viaje de regreso al agua dulce.");
     }
 
+    /**
+     * Método que muestra las acciones realizadas por el salmón durante un periodo de tiempo.
+     * Heredado de la clase Pez y sobreescrito para incluir la migración.
+     */
     @Override
     public void verAnimal() {
         Runnable[] acciones = {
